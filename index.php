41da0a4579d8548f813633432a21923063bd091f
lvhj <lvhjvip@gmail.com>
2024-12-01 19:45:43 +0800

    feat:add try catch

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 require_once __DIR__ . '/vendor/composer/autoload_real.php';
 ComposerAutoloaderInit5b16528999c1beb466aaefe9037c5b65::getLoader();
 
+try {
 
 
+} catch (Exception $e) {
+    dd($e);
+}
 
