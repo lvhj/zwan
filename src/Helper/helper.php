693f7a92d44518c6fbdec25a19d4744d5f8fee9e
lvhj <lvhjvip@gmail.com>
2024-12-04 10:20:37 +0800

    feat:fix helper

--- src/Helper/helper.php
+++ src/Helper/helper.php
@@ -4,7 +4,6 @@ if (!function_exists('dd')) {
     function dd(...$args)
     {
         ddPrint(...$args);
-        echo PHP_EOL;
         exit();
     }
 }
@@ -13,7 +12,6 @@ if (!function_exists('ddd')) {
     function ddd(...$args)
     {
         ddPrint(...$args);
-        echo PHP_EOL;
     }
 }
 
@@ -24,23 +22,24 @@ if (!function_exists('ddPrint')) {
         foreach ($args as $arg) {
             if ($arg instanceof Throwable) {
                 header('Content-type: application/xml;charset=utf-8;');
-                echo $arg->getMessage() . PHP_EOL;
                 echo PHP_EOL;
+                echo $arg->getMessage() . PHP_EOL;
                 echo $arg->getFile() . '(' . $arg->getLine() . ')' . PHP_EOL;
-                echo $arg->getTraceAsString() . PHP_EOL;
+                echo $arg->getTraceAsString();
                 exit();
             } elseif (is_array($arg) || is_object($arg)) {
                 echo json_encode($arg, 320);
             } else if (is_string($arg) || is_numeric($arg)) {
-                echo $arg . PHP_EOL;
+                echo $arg;
             } elseif (is_bool($arg)) {
                 $temp = $arg ? "TRUE" : "FALSE";
-                echo $temp . PHP_EOL;
+                echo $temp;
             } else if (is_null($arg)) {
-                echo 'NULL' . PHP_EOL;
+                echo 'NULL';
             } else {
                 var_dump($arg);
             }
+            echo PHP_EOL;
         }
     }
 }
