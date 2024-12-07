6a406cab7d8d82b1fbf50551531627dc62f10dc7
lvhj <lvhjvip@gmail.com>
2024-12-07 17:56:53 +0800

    feat:redis query

--- index.php
+++ index.php
@@ -8,18 +8,20 @@ try {
     $redis->connect('127.0.0.1', 6379);
     $redis->auth('123456');
 
-    \ZWan\Tool\Applications\RedisApplication::setRedis($redis);
-    $redisProxy = \ZWan\Tool\RedisProxy\Services\RedisProxy::getRedisProxy();
-
-    $redisProxy->set('key2', json_encode([
+    $redis->set('key2', json_encode([
         'id' => 1,
         'name' => 'name555'
     ]), 10000);
+
+    // 将redis连接注入到redisApplication
+    \ZWan\Tool\Applications\RedisApplication::setRedis($redis);
 
     $queryParam = new \ZWan\Tool\RedisProxy\Params\MultiQueryRedisParam();
     $queryParam->dataType = \ZWan\Tool\RedisProxy\Constants\RedisDataEnum::TYPE_STRING;
     $queryParam->keys = ['key1', 'key2', 'key4'];
     $queryParam->jsonArray = true;
+
+    $redisProxy = \ZWan\Tool\RedisProxy\Services\RedisProxy::getRedisProxy();
     $values = $redisProxy->multiQuery($queryParam);
     dd($values, 23);
 } catch (Exception $e) {
--- src/Tool/RedisProxy/Factory/Impl/HashMultiQueryRedisService.php
+++ src/Tool/RedisProxy/Factory/Impl/HashMultiQueryRedisService.php
@@ -2,16 +2,20 @@
 
 namespace ZWan\Tool\RedisProxy\Factory\Impl;
 
+use ZWan\Tool\Applications\RedisApplication;
 use ZWan\Tool\RedisProxy\Factory\MultiQueryRedisInterface;
 use ZWan\Tool\RedisProxy\Params\MultiQueryRedisParam;
 use ZWan\Tool\RedisProxy\Payload\MultiResultPayload;
 
 class HashMultiQueryRedisService implements MultiQueryRedisInterface
 {
+    private static $redis;
+
     public function multiQuery(MultiQueryRedisParam $multiQueryRedisParam): MultiResultPayload
     {
+        $redisApplication = RedisApplication::getRedis();
         // 使用管道批量获取值
-        $pipeline = self::$redis->multi(\Redis::PIPELINE);
+        $pipeline = self::$redisApplication->multi(\Redis::PIPELINE);
         foreach ($multiQueryRedisParam->keys as $key) {
             $pipeline->HMGET($key); // 添加 GET 命令到管道
         }
